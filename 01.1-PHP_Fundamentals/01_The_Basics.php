3e9d7d365632b59299eafad5c3796c6f8feb87ec
Michael MacDonagh <michaelgmacd@gmail.com>
2019-07-15 01:19:46 +0100

    Php Hello World

---
+++ 01.1-PHP_Fundamentals/01_The_Basics.php
@@ -0,0 +1,41 @@
+<!--Php stands for Php: Hypertext Preprocessor. -->
+<!-- It is a server-side language. -->
+
+<!-- Php must run on a server either Locally or in a Live environment like; XAMPP. -->
+
+<!-- Original Syntax: This is Standard Practice. -->
+<?php
+  // code  goe here.
+?>
+
+<!-- Short Syntax. -->
+<?
+  // code  goe here.
+?>
+
+<!-- Hello World -->
+<?
+ echo "Hello World!";
+?>
+<!-- Php  can be placed anywhere inside of a html element.-->
+<!-- Example: -->
+<div style='<?php echo "border: 1px solid #000"; ?>'>Black Border Box!</div>
+
+<!-- Quick Echo - Must be enabled on the server .-->
+<!-- Example: -->
+<?="My Text Goes Here"; ?>
+<!-- This will ONLY ever be used to echo .-->
+
+<!-- Semi-Colins: -->
+<!-- In Php  semi-colons i.e. ";" are mandatory. -->
+
+<!-- Comments: -->
+<?php
+//  01:  Single-line Comment
+#   02:  Single-line Comment
+/* 03:  Single-line
+Comment*/
+?>
+
+
+
